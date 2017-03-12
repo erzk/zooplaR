892ecf08c6c9572da8901c3cca83b0bd71f98ebc
Eryk <e.walczak@hotmail.com>
2017-03-12 22:34:10 +0000

    formatting

--- R/get_session_id.R
+++ R/get_session_id.R
@@ -2,14 +2,16 @@
 #'
 #' @param 
 #'
-#' @return
+#' @return A list
 #' @export
 #'
 #' @references \url{http://developer.zoopla.com/docs/read/Get_Session_ID}
 #'
 #' @examples
+#' get_session_id()
 #' 
-
 get_session_id <- function(){
-  
+  r <- GET("http://api.zoopla.co.uk/api/v1/get_session_id")
+  warn_for_status(r)
+  content(r, encoding="UTF-8")
 }
