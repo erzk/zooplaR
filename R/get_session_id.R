d98372e77e19d170982bf4c3fc865ff53e6611ea
Eryk <e.walczak@hotmail.com>
2017-03-14 00:11:03 +0000

    returns session ID string

--- R/get_session_id.R
+++ R/get_session_id.R
@@ -1,17 +1,24 @@
 #' Get Session ID
 #'
-#' @param NA
+#' @param API_key An API key from Zoopla.
 #'
-#' @return A list
+#' @return A string - session ID.
 #' @export
 #'
 #' @references \url{http://developer.zoopla.com/docs/read/Get_Session_ID}
 #'
 #' @examples
-#' get_session_id()
+#' get_session_id("YOUR_API_KEY")
 #' 
-get_session_id <- function(){
-  r <- GET("http://api.zoopla.co.uk/api/v1/get_session_id")
+get_session_id <- function(API_key=NULL){
+  if (!is.character(API_key)) {
+    stop("Please provide an API key.")
+  }
+  r <- GET(paste0("http://api.zoopla.co.uk/api/v1/get_session_id?api_key=", API_key))
   warn_for_status(r)
-  content(r, encoding="UTF-8")
+  r %>% content(encoding="UTF-8") %>%
+    xmlParse() %>%
+    xmlToList() %>%
+    .$session_id %>%
+    return()
 }
