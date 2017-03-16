39267b200d8a27c73c03560987e0257fcff23192
Eryk <e.walczak@hotmail.com>
2017-03-16 23:18:03 +0000

    first commit - functional

--- R/local_info_graphs.R
+++ R/local_info_graphs.R
@@ -1,5 +1,6 @@
 #' Local Info Graphs
 #'
+#' @param area A string representing a valid UK outcode, e.g. "W12".
 #' @param API_key An API key from Zoopla.
 #'
 #' @return A list.
@@ -8,8 +9,20 @@
 #' @references \url{http://developer.zoopla.com/docs/read/Local_Info_Graphs}
 #'
 #' @examples
+#' local_info_graphs("W12", "YOUR_API_KEY")
 #' 
-
-local_info_graphs <- function(){
-  
+local_info_graphs <- function(area=NULL, API_key=NULL){
+  if (!is.character(area) || nchar(area) < 2) {
+    stop("Please provide a valid UK outcode. Incode is optional.")
+  }
+  if (!is.character(API_key)) {
+    stop("Please provide a valid API key. It must be a string.")
+  }
+  r <- GET("http://api.zoopla.co.uk/api/v1/local_info_graphs",
+           query = list(area = area, api_key = API_key))
+  warn_for_status(r)
+  r %>% content(encoding="UTF-8") %>%
+    xmlParse() %>%
+    xmlToList() %>%
+    return()
 }
