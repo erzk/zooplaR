cdc4c3fab1a2874bc88ba23407a44e5592461a0a
Eryk <e.walczak@hotmail.com>
2017-03-18 00:39:41 +0000

    reordered arguments

--- R/area_zed_indices.R
+++ R/area_zed_indices.R
@@ -1,6 +1,12 @@
 #' Area Zed-Indices
 #'
+#' @param postcode A string. Valid UK postcode.
+#' @param area_type A string. Area type: "postcodes", "streets", "outcodes", "areas", "towns", "counties". Read the API documentation for details.
+#' @param output_type A string. Required output: "outcode", "area", "town", "county", "country". Read the API documentation for details.
+#' @param page_number An integer. The page number of results to request, default is 1.
+#' @param page_size An integer. The size of each page of results, default is 10, maximum is 20.
 #' @param API_key An API key from Zoopla.
+#' @param ordering A string. The order in which the results should be returned in, either "descending" (default) or "ascending".
 #'
 #' @return A list.
 #' @export
@@ -10,12 +16,12 @@
 #' @examples
 #' \dontrun{
 #' 
-#' area_zed_indices("descending", "sw185rw", "postcodes", "outcode", 1, 10, "YOUR_API_KEY")
+#' area_zed_indices("sw185rw", "postcodes", "outcode", 1, 10, "YOUR_API_KEY", "descending")
 #' area_zed_indices(postcode = "sw185rw", area_type = "postcodes", output_type = "outcode", API_key = "YOUR_API_KEY")
 #' }
 #' 
-area_zed_indices <- function(ordering="descending", postcode=NULL, area_type=NULL, output_type=NULL,
-                             page_number=NULL, page_size=NULL, API_key=NULL){
+area_zed_indices <- function(postcode=NULL, area_type=NULL, output_type=NULL,
+                             page_number=NULL, page_size=NULL, API_key=NULL, ordering="descending"){
   if (!is.character(postcode) || nchar(postcode) < 2) {
     stop("Please provide a valid UK area.")
   }
@@ -26,8 +32,9 @@ area_zed_indices <- function(ordering="descending", postcode=NULL, area_type=NUL
   #http://api.zoopla.co.uk/api/v1/zed_indices.xml?api_key=xxxxxx&postcode=DE39AH&output_type=outcode&area_type=postcodes
   #http://api.zoopla.co.uk/api/v1/zed_indices?api_key=xxxxxxxx&postcode=DE39AH&output_type=outcode&area_type=streets
   r <- GET("http://api.zoopla.co.uk/api/v1/zed_indices",
-           query = list(ordering = ordering, postcode = postcode, area_type = area_type, output_type = output_type,
-                        page_number = page_number, page_size = page_size, api_key = API_key))
+           query = list(postcode = postcode, area_type = area_type, output_type = output_type,
+                        page_number = page_number, page_size = page_size,
+                        api_key = API_key, ordering = ordering))
   warn_for_status(r)
   r %>% content(encoding="UTF-8") %>%
     xmlParse() %>%
