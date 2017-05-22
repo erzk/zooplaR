52324ee7f69dfc2bb33ba4af8cd43a5c36e503e0
Eryk <e.walczak@hotmail.com>
2017-05-22 20:54:32 +0100

    cleaned/working version

--- R/property_listings.R
+++ R/property_listings.R
@@ -16,18 +16,13 @@
 #' @examples
 #' \dontrun{
 #' 
-#' property_listings()
+#' property_listings(postcode = "E1", API_key = APIKEY)
+#' property_listings(area = "Nottingham", API_key = APIKEY)
 #' }
 #' 
 property_listings <- function(postcode=NULL, area=NULL, API_key=NULL){
-  #GET("http://api.zoopla.co.uk/api/v1/property_listings.xml?postcode=E15&api_key=") # also works with full postcodes
-  #GET("http://api.zoopla.co.uk/api/v1/property_listings.xml?area=Nottingham&api_key=")
-
-  #GET("http://api.zoopla.co.uk/api/v1/property_listings.xml", 
-  #query = list(api_key = "value1", postcode = "value2", area = "value3")
-  #)
-  if (!is.character(postcode) || nchar(postcode) < 2) {
-    stop("Please provide a valid UK area name. It should be unambiguous.")
+  if (missing(postcode) || missing(area)) {
+    stop("Please provide a valid UK postcode or area name. It should be unambiguous.")
   }
   if (!is.character(API_key)) {
     stop("Please provide an API key.")
