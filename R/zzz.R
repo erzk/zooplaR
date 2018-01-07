6f9e415f20e4e0691f59d24365a0058a2d81ba6f
Eryk <e.walczak@hotmail.com>
2018-01-07 00:03:39 +0000

    first commit

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+# skip testing functions that need authentication
+# https://cran.r-project.org/web/packages/httr/vignettes/secrets.html
+skip_if_no_auth <- function() {
+  if (identical(Sys.getenv("zoopla_key"), "")) {
+    testthat::skip("No authentication available")
+  }
+}
+
+# TODO add packages
+return_content <- function(x) {
+  warn_for_status(x)
+  
+  if (httr::status_code(x) == 200) {
+    x %>%
+    content(encoding = "UTF-8") %>%
+    xmlParse() %>%
+    xmlToList() %>%
+    return()
+  }
+}
